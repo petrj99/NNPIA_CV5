63f8d04d97cfbc863d2f673def7c86868f5fd64d
Petr Jiruše <petrjiruse@seznam.cz>
2024-04-09 17:38:01 +0200

    Add task 5d

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,18 +6,10 @@ function App() {
     return (
         <div className="app">
             <h1>Základní koncepty Reactu</h1>
-            {data.map((concept, index) => (
-                index % 2 === 0 ? (
-                    // Klasické props a destructuring
-                    <ReactConcept key={index} title={concept.title} description={concept.description} />
-                ) : (
-                    // Children prop
-                    <ReactConcept key={index}>
-                        <h2>{concept.title}</h2>
-                        <p>{concept.description}</p>
-                    </ReactConcept>
-                )
-            ))}
+            <ReactConcept title={data[0].title} description={data[0].description} />
+            <ReactConcept title={data[1].title} description={data[2].description} />
+            <ReactConcept title={data[2].title} description={data[2].description} />
+            <ReactConcept title={data[3].title} description={data[3].description} />
         </div>
     );
 }
