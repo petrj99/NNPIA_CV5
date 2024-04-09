df5d07a5349ad94bab6e41f88e2f0693817ac267
Petr Jiruše <petrjiruse@seznam.cz>
2024-04-09 19:11:07 +0200

    Add task 5f

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactConcept from './components/ReactConcept';
 import { data } from './init-data';
 
 function App() {
     return (
-        <div className="app">
-            <h1>Základní koncepty Reactu</h1>
-            <ReactConcept title={data[0].title} description={data[0].description} image={process.env.PUBLIC_URL + data[0].image}  />
-            <ReactConcept title={data[1].title} description={data[1].description} image={process.env.PUBLIC_URL + data[1].image}  />
-            <ReactConcept title={data[2].title} description={data[2].description} image={process.env.PUBLIC_URL + data[2].image}  />
-            <ReactConcept title={data[3].title} description={data[3].description} image={process.env.PUBLIC_URL + data[3].image}  />
+        <div className="container">
+            <div className="row">
+                {data.map((concept, index) => (
+                    <div key={index} className="col-sm-12 col-md-6 col-lg-4">
+                        <ReactConcept
+                            title={concept.title}
+                            description={concept.description}
+                            image={concept.image}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- frontend/src/components/ReactConcept.js
+++ frontend/src/components/ReactConcept.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function ReactConcept({ title, description, image }) {
     return (
-        <div className="react-concept">
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <img src={image} alt={title} style={{maxWidth: '100%', height: 'auto'}}/>
+        <div className="card" style={{ width: '18rem' }}>
+            <img src={image} className="card-img-top" alt={title} />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+            </div>
         </div>
     );
 }
