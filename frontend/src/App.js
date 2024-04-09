4ceeea9928dfdcb3b30271d1f705800a41a6a5e3
Petr Jiruše <petrjiruse@seznam.cz>
2024-04-09 17:18:21 +0200

    Add task 5a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import ReactConcept from './components/ReactConcept';
 
 function App() {
-  return (
-      <div className="App">
-        <ReactConcept
-            title="Komponenty a props"
-            description="Testovaci text reprezentujici popis"
-        />
-      </div>
-  );
+    const concepts = [
+        { title: "Komponenty", description: "Popis 1" },
+        { title: "Props", description: "Popis 2" },
+        { title: "Stav", description: "Popis 3" },
+        { title: "Životní cyklus", description: "Popis 4" }
+    ];
+
+    return (
+        <div className="app">
+            <h1>Základní koncepty Reactu</h1>
+            {concepts.map((concept, index) => (
+                <ReactConcept key={index} title={concept.title} description={concept.description} />
+            ))}
+        </div>
+    );
 }
 
 export default App;
